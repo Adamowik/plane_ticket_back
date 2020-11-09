4c2e64605ca0130730cd1b84f68fba6d60be77eb
Rkull <baiskey.developer@gmail.com>
2020-11-09 19:57:33 +0100

    Switching off emails

--- src/main/java/com/plane/tickets/project/sellingplanetickets/controllers/TicketController.java
+++ src/main/java/com/plane/tickets/project/sellingplanetickets/controllers/TicketController.java
@@ -1,14 +1,18 @@
 package com.plane.tickets.project.sellingplanetickets.controllers;
 
 import com.plane.tickets.project.sellingplanetickets.DTO.TicketDTO;
-import com.plane.tickets.project.sellingplanetickets.model.Ticket;
 import com.plane.tickets.project.sellingplanetickets.services.NotificationService;
 import com.plane.tickets.project.sellingplanetickets.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -45,13 +49,13 @@ public class TicketController {
     @PostMapping(value = "/tickets")
     public void addTicket(@RequestBody TicketDTO ticket) {
         ticketService.addTicket(ticket);
-        try {
-            notificationService.sendTicket(ticket);
-        } catch (MailException e) {
-            System.out.println("Wysylanie maia nieudane");
-        } catch (ParseException f) {
-            System.out.println("Zamiana daty nieudana");
-        }
+//        try {
+//            notificationService.sendTicket(ticket);
+//        } catch (MailException e) {
+//            System.out.println("Wysylanie maia nieudane");
+//        } catch (ParseException f) {
+//            System.out.println("Zamiana daty nieudana");
+//        }
     }
 
     @PutMapping(value = "/tickets/{id}")
